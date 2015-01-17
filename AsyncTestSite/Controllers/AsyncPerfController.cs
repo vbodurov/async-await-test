98f20a8a103f4d5801219406800783fbcd6b53de
vbodurov <vladimir@bodurov.com>
2015-01-16 23:29:08 -0800

    iis

--- AsyncTestSite/Controllers/AsyncPerfController.cs
+++ AsyncTestSite/Controllers/AsyncPerfController.cs
@@ -2,20 +2,23 @@
 using System.Net;
 using System.Threading.Tasks;
 using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.SessionState;
 
 namespace AsyncTestSite.Controllers
 {
+    [SessionState(SessionStateBehavior.Disabled)]
     public class AsyncPerfController : ApiController
     {
-        [Route("api/async")]
-        [HttpGet]
+        [System.Web.Http.Route("api/async")]
+        [System.Web.Http.HttpGet]
         public async Task<string> Get()
         {
             var sw = Stopwatch.StartNew();
 
             var r =
                 await new WebClient()
-                    .DownloadStringTaskAsync("http://ma.youvisio.com/api/version?os=android");
+                    .DownloadStringTaskAsync("http://ma.youvisio.com/api/fake");
             
             sw.Stop();
             r += " ms:" + sw.ElapsedMilliseconds;
--- AsyncTestSite/Controllers/SyncPerfController.cs
+++ AsyncTestSite/Controllers/SyncPerfController.cs
@@ -1,21 +1,24 @@
 ﻿using System.Diagnostics;
 using System.Net;
 using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.SessionState;
 
 namespace AsyncTestSite.Controllers
 {
+    [SessionState(SessionStateBehavior.Disabled)]
     public class SyncPerfController : ApiController
     {
 
-        [Route("api/sync")]
-        [HttpGet]
+        [System.Web.Http.Route("api/sync")]
+        [System.Web.Http.HttpGet]
         public string Get()
         {
             var sw = Stopwatch.StartNew();
 
             var r = 
                 new WebClient()
-                    .DownloadString("http://ma.youvisio.com/api/version?os=android");
+                    .DownloadString("http://ma.youvisio.com/api/fake");
 
             sw.Stop();
             r += " ms:" + sw.ElapsedMilliseconds;
